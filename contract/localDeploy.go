72d0767c3bfc4c86c51fd74905e7635da763adda
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-05-30 13:49:53 -0400

    Added the ganache private keys

--- contract/localDeploy.go
+++ contract/localDeploy.go
@@ -1,12 +1,16 @@
 package contract
 
+// import (
+// 	"context"
 // "github.com/kimanikelly/cli-ttBank/signer"
 // "github.com/ethereum/go-ethereum/accounts/abi/bind"
-func DeployContracts() {
+// )
 
-	// address,privateKey := signer.Wallet()
-	// chainId,err := contract.ProviderConnection().ChainID(context.Background())
+// func DeployContracts() {
 
-	// // Binds the connected signer to the transaction options
-	// auth, authErr := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-}
+// 	address,privateKey := signer.Wallet()
+// 	chainId,err := contract.ProviderConnection().ChainID(context.Background())
+
+// 	// // Binds the connected signer to the transaction options
+// 	// auth, authErr := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+// }
--- utils/transactionOps.go
+++ utils/transactionOps.go
@@ -12,7 +12,7 @@ func ChainID() *big.Int {
 	chainId, err := client.ClientConnection().ChainID(context.Background())
 
 	if err != nil {
-		log.Fatalf("Failed to chainID %v", err)
+		log.Fatalf("Failed to return the chainID %v", err)
 	}
 
 	log.Println(chainId)
