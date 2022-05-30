629264f2b46c5903da51d80d737c623d9705ce7f
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-05-30 12:37:38 -0400

    Added the signer package and the Wallet function to access the connected wallet

---
+++ signer/signer.go
@@ -0,0 +1,41 @@
+package signer
+
+import (
+	"crypto/ecdsa"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func Wallet() (common.Address, *ecdsa.PrivateKey) {
+
+	//
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Failed to load the .env file %v", err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(os.Getenv("PRIVATE_KEY"))
+
+	if err != nil {
+		log.Fatalf("Failed to parse the private key %v", err)
+	}
+
+	publicKey := privateKey.Public()
+
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+
+	if !ok {
+		log.Fatal("Failed assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	signerAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	return signerAddress, privateKey
+
+}
