07d5a94cccfb94d66afbf418d422fc2add4c7db4
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-06-08 11:24:58 -0400

    Imported the signerAddress and privateKey into app.go

--- app/app.go
+++ app/app.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 
 	"github.com/kimanikelly/cli-ttBank/contract"
+	"github.com/kimanikelly/cli-ttBank/signer"
 )
 
 func StartApp() {
 
+	// Returns the TTBank.sol contract instance
 	ttBank := contract.ContractInstance()
 
 	fmt.Println(ttBank)
+
+	signerAddress, privateKey := signer.Wallet()
+
+	fmt.Println(signerAddress, privateKey)
+
 }
